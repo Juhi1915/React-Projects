a150f4ffcbfa22be73242916a2b15661d64e89f1
Juhi <jbhardwaj@bhavnacorp.com>
2019-09-23 18:39:57 +0530

    Registration form

---
+++ React -Registration-Login-Form/public/image.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { image } from 'react'
+
+const image = () => (
+   <Image source = {require('C:\Program Files\nodejs\react-test\logo.svg')} />
+)
+export default image
